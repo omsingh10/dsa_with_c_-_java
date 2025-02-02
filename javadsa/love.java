366a75fd66e9543ddc874a619bb5854d4ee16fab
omsingh10 <os0106897@gmail.com>
2025-02-02 20:10:23 +0530

    humans are silly

--- javadsa/love.java
+++ javadsa/love.java
@@ -2,7 +2,8 @@ package javadsa;
 
 public class love {
     public static void main(String[] args) {
-        System.out.println("for checking the path");
+
+        System.out.println("I love you");
     }
     
 }
---
+++ javadsa/yelo.java
@@ -0,0 +1,7 @@
+public class yelo {
+        public static void main(String[] args) {
+    
+            System.out.println("I love you shiv");
+        }
+        
+    }
