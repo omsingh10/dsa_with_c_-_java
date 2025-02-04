1622f23ac44010d391f77aac3ae1ab1bf223d245
omsingh10 <os0106897@gmail.com>
2025-02-05 00:23:52 +0530

    commit kar

--- javadsa/pattern1.java
+++ javadsa/pattern1.java
@@ -1,2 +1,21 @@
-package javadsa;public class pattern1 {
+package javadsa;
+
+public class pattern1 {
+    public static void main(String[] args) {
+
+        System.out.println("Pattern 1 : square");
+        for (int i = 0; i < 5; i++) { // outer loop for number of rows(n)
+
+            for (int j = 0 ; j <= 5 ; j++) { // inner loop for number of columns
+                
+                    System.out.print("*");
+                
+   
+            }
+            System.out.println(" ");
+
+            
+        }
+
+}
 }
--- javadsa/yelo.java
+++
@@ -1,7 +0,0 @@
-public class yelo {
-        public static void main(String[] args) {
-    
-            System.out.println("I love you shiv");
-        }
-        
-    }
