2160f027fb7ba1f6d9bf57dfb75217bdfdf155f1
omsingh10 <os0106897@gmail.com>
2025-02-06 02:14:13 +0530

    yelo 3

--- javadsa/pattern3.java
+++ javadsa/pattern3.java
@@ -1,2 +1,28 @@
-package javadsa;public class pattern3 {
+package javadsa;
+
+import java.sql.SQLOutput;
+
+public class pattern3 {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 5; i++) {
+            // Print spaces
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println(); // Move to the next line
+        }
+
+
+
+
+    }
+
+
+
 }
