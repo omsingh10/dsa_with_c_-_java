ded6e853e42d4ea08b0c8df5dd59d84caddbe79d
omsingh10 <os0106897@gmail.com>
2025-02-06 00:24:09 +0530

    addkardoplease

--- javadsa/pattern1.java
+++ javadsa/pattern1.java
@@ -4,9 +4,9 @@ public class pattern1 {
     public static void main(String[] args) {
 
         System.out.println("Pattern 1 : square");
-        for (int i = 0; i < 5; i++) { // outer loop for number of rows(n)
+        for (int i = 0; i < 5; i++) {
 
-            for (int j = 0 ; j <= 5 ; j++) { // inner loop for number of columns
+            for (int j = 0 ; j <= i ; j++) {
                 
                     System.out.print("*");
                 
