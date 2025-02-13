1e78be88b800de05fafd529b05d015592f673fdb
omsingh10 <os0106897@gmail.com>
2025-02-14 05:10:25 +0530

    this is string problem done

---
+++ javadsa/Solution2.java
@@ -0,0 +1,22 @@
+package javadsa;
+import java.util.*;
+
+
+
+
+
+public class Solution2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+
+        String rel = S.substring(start, end);
+
+
+        System.out.println(rel);
+
+
+    }
+    }
