b610f11251ceb2acd6129923deea101802930aac
omsingh10 <os0106897@gmail.com>
2025-10-21 19:32:09 +0530

    humns

---
+++ javadsa/human.java
@@ -0,0 +1,12 @@
+public class human {
+
+    public static void main(String[] args) {
+        System.out.println("  O  ");
+        System.out.println(" /|\\ ");
+        System.out.println(" / \\ ");
+    }
+    
+   
+
+
+}
