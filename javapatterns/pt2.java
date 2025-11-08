75d4ddebb4df7112341ea43f33d6462fac70870d
omsingh10 <os0106897@gmail.com>
2025-11-08 15:00:28 +0530

    yew

--- javapatterns/pt2.java
+++ javapatterns/pt2.java
@@ -5,7 +5,8 @@ public class pt2 {
     public static void main(String[] args) {
         System.out.println("this is part 2");
 
-        System.err.println("ka");
+        System.err.println("human");
+
     }
     
 }
