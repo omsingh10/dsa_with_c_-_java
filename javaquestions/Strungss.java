a6092f60258823ad9384707657c46053b246b324
omsingh10 <os0106897@gmail.com>
2025-11-11 00:45:02 +0530

    Stings-1

---
+++ javaquestions/Strungss.java
@@ -0,0 +1,15 @@
+package javaquestions;
+
+import java.util.Scanner;
+
+public class Strungss {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println(name);
+       
+    }
+    
+}
