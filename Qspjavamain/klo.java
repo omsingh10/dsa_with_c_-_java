bde4ee490a2d85d530bcad64e6d276f14e1a97fa
omsingh10 <os0106897@gmail.com>
2026-02-12 22:24:16 +0530

    human

---
+++ Qspjavamain/klo.java
@@ -0,0 +1,30 @@
+public class klo {
+
+static class Dog{
+
+    String name;
+    int age;
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
+class Demo{
+    public static void main(String[] args) {
+
+        Dog dog1 = new Dog("Buddy", 3);
+        Dog dog2 = new Dog("Max", 5);
+
+        System.out.println("Dog 1 humsn: " + dog1.name + ", Age: " + dog1.age )  ;
+        System.out.println("Dog 2: " + dog2.name + ", Age: " + dog2.age ) ;
+
+    }
+}
+
+
+
+
+
+}
