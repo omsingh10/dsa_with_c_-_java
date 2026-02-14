399e341bcadef137346f84d9f0382c13e481063d
omsingh10 <os0106897@gmail.com>
2026-02-14 21:32:18 +0530

    wow

--- Qspjavamain/wowow.java
+++ Qspjavamain/wowow.java
@@ -1,9 +1,32 @@
 public class wowow {
+   
+    
+     static class Classroom{
 
-    public static void main(String[] args){
+        String  stname;
 
-        System.out.println("Hello World");
-        
+        static int stnumber;
+        static String stteach = "sharma mam";
+
+        public Classroom(String stname) {
+            this.stname = stname;
+            stnumber++;
+        }
+
     }
 
+    public static void main(String[] args) {
+ 
+        Classroom c1 = new Classroom("satyarth");
+        Classroom c2 = new Classroom("satyarth2");
+        Classroom c3 = new Classroom("satyarth5");
+
+        System.out.println(c1.stname);
+        System.out.println(c2.stname);
+        System.out.println(c3.stname);
+
+        System.out.println(Classroom.stnumber);
+        System.out.println(Classroom.stteach);
+        
+    }
 }
