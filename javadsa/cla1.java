b9888e46a31c5807a4a0ea4581ba6a3031c4ce2a
omsingh10 <os0106897@gmail.com>
2026-02-15 17:41:53 +0530

    java

--- javadsa/cla1.java
+++ javadsa/cla1.java
@@ -1,9 +1,5 @@
 public class cla1 {
     public static void main(String[] args) {
-         
-        word1:  a   b   c
-word2:    p   q   r
-merged: a p b q c r
 
 
 
--- javadsa/pattern2.java
+++ javadsa/pattern2.java
@@ -1,4 +1,3 @@
-package javadsa;
 
 public class pattern2 {
     public static void main(String[] args) {
