0dc2ced1060f0c0676ba582d6ec46242fc132f8b
omsingh10 <os0106897@gmail.com>
2026-02-16 23:35:04 +0530

    s

--- javadsa/javadsa/pattern1.java
+++ javadsa/javadsa/pattern1.java
@@ -8,7 +8,7 @@ public class pattern1 {
 
             for (int j = 0 ; j <= i ; j++) {
                 
-                    System.out.print("*");
+                    System.out.print(" * ");
                 
 
                     
